97a956a8b4d84964568c16c18dd6be0b19cc696e
Yunsung Oh <henry.oh@utoronto.ca>
2019-03-27 02:41:02 -0400

    add test page as root v2

--- app/controllers/api/ratings_controller.rb
+++ app/controllers/api/ratings_controller.rb
@@ -7,7 +7,7 @@ class Api::RatingsController < ApplicationController
     @question = Question.find(@answer.question_id)
 
     if current_user.id == @question.user_id || current_user.id == @answer.user_id
-      if (@rating = Rating.where(answer_id: @answer.id))
+      if (@rating = Rating.where(answer_id: @answer.id).first)
         render :json => @rating
       else
         render :json => nil
@@ -20,7 +20,8 @@ class Api::RatingsController < ApplicationController
   def create
     @answer = Answer.find(params[:answer_id])
     @question = Question.find(@answer.question_id)
-    if @question && current_user.id == @question.user_id
+    r_value = params[:value].to_i
+    if @question && current_user.id == @question.user_id && r_value >= 0 && r_value <= 10
       @rating = @answer.ratings.new(
         value: params[:value],
         user_id: @answer.user_id
