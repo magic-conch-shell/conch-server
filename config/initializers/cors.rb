0f85f3156cacde900d5c2d290201054a68d8c011
Scott Donnelly <scoot.donnelly@gmail.com>
2019-04-09 09:14:38 -0400

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://magic-conch-shell.github.io/conch-web/'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
