9b1e0cc9a8fbc71dab4033e6f8ff84b6c75a50dd
Yunsung Oh <henry.oh@utoronto.ca>
2019-04-09 14:37:17 -0400

    fix header response v4

--- app/controllers/api/tags_controller.rb
+++ app/controllers/api/tags_controller.rb
@@ -1,5 +1,6 @@
 class Api::TagsController < ApplicationController
   skip_before_action :verify_authenticity_token
+  before_action :set_headers
 
   def index
     render :json => Tag.all, status: 200
